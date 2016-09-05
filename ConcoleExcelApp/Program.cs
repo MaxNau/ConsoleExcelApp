49ee91ec0ebfe5515737d131b10c1927ef337a80
Rionis <Rionis@DESKTOP-IFU25EE>
2016-09-05 13:51:10 +0300

    Added separet class for table: Added indexer; Added atributes for Row and Column length.
    Added more user input validations.
    Continued refactoring code.

--- ConcoleExcelApp/Program.cs
+++ ConcoleExcelApp/Program.cs
@@ -1,20 +1,38 @@
 ﻿using System;
+using System.Diagnostics.CodeAnalysis;
 
 namespace ConcoleExcelApp
 {
     class Program
     {
+        [SuppressMessage("ReSharper", "UnusedParameter.Local")]
         static void Main(string[] args)
         {
-            Console.WriteLine("Pleas enter table size:\n ");
-            Console.Write("Pleas enter number of rows: ");
-            var rows = int.Parse(Console.ReadLine());
-            Console.Write("Pleas enter number of columns: ");
-            var columns = int.Parse(Console.ReadLine());
+            Console.WriteLine("Pleas, enter table size: ");
+            Console.Write("Enter number of rows: ");
+
+            int rows;
+
+            while (!int.TryParse(Console.ReadLine(), out rows))
+            {
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine("Must be an integer");
+                Console.ResetColor();
+                Console.Write("Enter number of rows: ");
+            }
+
+            int columns;
+
+            while (!int.TryParse(Console.ReadLine(), out columns))
+            {
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine("Must be an integer");
+                Console.ResetColor();
+                Console.Write("Enter number of columns: ");
+            }
 
             var table = new Table(rows, columns);
 
-           // table.ValidateFormula("1A");
             table.PopulateTable();
             table.CalculateResult();
             table.DisplayTable(true);
--- ConcoleExcelApp/Table.cs
+++ ConcoleExcelApp/Table.cs
@@ -1,68 +1,97 @@
 ﻿using System;
 using System.Data;
+using System.Globalization;
 using System.Linq;
-using Microsoft.SqlServer.Server;
 
 namespace ConcoleExcelApp
 {
     public class Table
     {
-        private readonly Cell[,] _table;
+        private readonly ExcelTable _excelTable;
 
         public Table(int rows, int columns)
         {
-            _table = new Cell[rows, columns];
+            _excelTable = new ExcelTable(rows, columns);
         }
 
-        // populates table with user input
-        // if user doesn't fill all cells, each cell without input trited as empty
+        // inner class that holds the table
+        private class ExcelTable
+        {
+            private const int ColumnIndocator = 0;
+            private const int RowIndicator = 1;
+
+            private readonly Cell[,] _table;
+
+            public ExcelTable(int rows, int columns)
+            {
+                _table = new Cell[rows, columns];
+            }
+
+            public Cell this[int columnIndex, int rowIndex]
+            {
+                get { return _table[columnIndex, rowIndex]; }
+                set { _table[columnIndex, rowIndex] = value; }
+            }
+
+            // Returns number of columns.
+            public int GetColumnLength => _table.GetLength(ColumnIndocator);
+
+            // Returns number of rows.
+            public int GetRowLength => _table.GetLength(RowIndicator);
+        }
+
+        
+        // Populates table with user input.
+        // If user doesn't fill all cells - cells without input trited as empty.
         public void PopulateTable()
         {
-            // temporary variable for storing user input
-
-            for (int col = 0; col < _table.GetLength(0); col++)
+            for (int col = 0; col < _excelTable.GetColumnLength; col++)
             {
-                // read user input and store it in temporary variable
-                var temp = Console.ReadLine();
-                // split user input string into seprate strings that correspond to cells
-                var nums = temp.Split(' ');
+                // Read user input and store it in temporary variable
+                var userInput = Console.ReadLine();
 
-                    for (int row = 0; row < _table.GetLength(1); row++)
-                    {
-                    // check if user filled all the cells through input
-                    // if check is valid then define cell data type
-                    // else triet cell as empty
-                    if (nums.Length > row)
-                    {
-                        _table[col, row] = CheckCellDataType(nums[row]);
-                    }
-                    else
-                        _table[col, row] = CheckCellDataType("");
+                // Split user input string into seprate strings that correspond to row cells
+                if (userInput != null)
+                {
+                    var cellData = userInput.Split(' ');
 
-                        // define the index of the cell in Excel format
-                        _table[col, row].Index = string.Format("{1}{0}", col + 1, NumberToAlpha(row));
+                    for (int row = 0; row < _excelTable.GetRowLength; row++)
+                    {
+                        // Check if user filled all the cells through input.
+                        // If check is valid then define cell data type
+                        // else triet cell as empty
+                        if (cellData.Length > row)
+                        {
+                            _excelTable[col, row] = CheckCellDataType(cellData[row]);
+                        }
+                        else
+                            _excelTable[col, row] = CheckCellDataType("");
+
+                        // Define the index of the cell in Excel format
+                        _excelTable[col, row].Index = string.Format("{1}{0}", col + 1, NumberToAlpha(row));
                     }
                 }
+            }
     
             DisplayTable();
             CalculateResult();
         }
 
-        // determines the data type of the cell
-        public Cell CheckCellDataType(string data)
+        // Determines the data type of the cell
+        private Cell CheckCellDataType(string data)
         {
             Cell cell = new Cell(data);
 
             int iresult;
 
-            // check if cell is empty
-            if (data == "")
+            // Check if cell is empty
+            if (string.IsNullOrEmpty(data))
             {
                 cell.DataType = CellDataType.Empty;
                 return cell;
             }
 
-            // check if cell contains digit, formula, text or cell contains invalid user input
+            // Check if cell contains digit, formula, text or cell contains invalid user input
             if (int.TryParse(data, out iresult))
             {
                 cell.DataType = CellDataType.Numeric;
@@ -75,21 +104,23 @@ namespace ConcoleExcelApp
             {
                 cell.DataType = CellDataType.Text;
             }
-
-            string error;
-
-            if (!ValidateUserInput(cell, out error))
+            else
             {
-                cell.DataType = CellDataType.Error;
-                cell.Data = error;
-                return cell;
+                string error;
+
+                if (!ValidateUserInput(cell, out error))
+                {
+                    cell.DataType = CellDataType.Error;
+                    return cell;
+                }
             }
+            
 
             return cell;
         }
 
-        // transfors alpha to the english alphabet letter
-        public string NumberToAlpha(long number, bool isLower = false)
+        // Transforms number to the english alphabet letter
+        private string NumberToAlpha(long number, bool isLower = false)
         {
             string returnVal = "";
             char c = isLower ? 'a' : 'A';
@@ -103,60 +134,57 @@ namespace ConcoleExcelApp
             return returnVal;
         }
 
-        public int AlphaToNumber(string alpha, bool isLower = false)
+        // Transforms english alphabet letter to number
+        private int AlphaToNumber(string alpha)
         {
-            int retVal = 0;
+            int returnVal = 0;
             string col = alpha.ToUpper();
             for (int iChar = col.Length - 1; iChar >= 0; iChar--)
             {
                 char colPiece = col[iChar];
                 int colNum = colPiece - 64;
-                retVal = retVal + colNum * (int)Math.Pow(26, col.Length - (iChar + 1));
+                returnVal = returnVal + colNum * (int)Math.Pow(26, col.Length - (iChar + 1));
             }
-            return retVal;
+            return returnVal;
         }
 
-        // searches for the links in cells and return the value that is located in corresponding cell
-        public object SearchIndex(string index)
+        // Searches for the links in cells and returns the value that is located in corresponding cell.
+        // If index of the cell corresponds to the index that we search check the cell data type.
+        // If cell data type is text or number then return the data in found cell.
+        // Else, if cell data type is formula check if formula can be evaluated (all links are replaced with values).
+        // If formula can be evaluated calculate and return cell data,
+        // if not search for the links and replace them with values until formula can be evaluated.
+        private object SearchIndex(string index)
         {
-            for (int c = 0; c < _table.GetLength(0); c++)
+            for (int c = 0; c < _excelTable.GetColumnLength; c++)
             {
-                for (int r = 0; r < _table.GetLength(1); r++)
+                for (int r = 0; r < _excelTable.GetRowLength; r++)
                 {
-                    // if index of the cell corresponds to the index that we search and cell data type is not a formula
-                    // than return the data from the found cell
-                    // else if index of the cell corresponds to the index that we search and cell data type is text
-                    // then return the found cell
-                    // else if index of the cell corresponds to the index that we search and cell data type is formula
-                    // check if formula can be evaluated (all links are replaced with values)
-                    // if formula can be evaluated return the result
-                    // if not search for the links and replace them with values until formula can be evaluated
-
-                    if (_table[c, r].Index == index)
+                    if (_excelTable[c, r].Index == index)
                     {
-                        if (_table[c, r].DataType == CellDataType.Numeric || _table[c, r].DataType == CellDataType.Text)
+                        if (_excelTable[c, r].DataType == CellDataType.Numeric || _excelTable[c, r].DataType == CellDataType.Text)
                         {
-                            return _table[c, r].Data;
+                            return _excelTable[c, r].Data;
                         }
-                        else if (_table[c, r].DataType == CellDataType.Formula)
+                        else if (_excelTable[c, r].DataType == CellDataType.Formula)
                         {
-                            if (CheckFormula(_table[c, r].Data))
+                            if (CheckFormula(_excelTable[c, r].Data))
                             {
-                                _table[c, r].Data = Convert.ToDouble(new DataTable().Compute((_table[c, r].Data).Substring(1), null)).ToString();
-                                _table[c, r].DataType = CellDataType.Numeric;
-                                return _table[c, r].Data;
+                                _excelTable[c, r].Data = Convert.ToDouble(new DataTable().Compute((_excelTable[c, r].Data).Substring(1), null)).ToString(CultureInfo.InvariantCulture);
+                                _excelTable[c, r].DataType = CellDataType.Numeric;
+                                return _excelTable[c, r].Data;
                             }
                             else
                             {
-                                string[] indexes = _table[c, r].Data.Split('+', '-', '/', '*', '=');
+                                string[] indexes = _excelTable[c, r].Data.Split('+', '-', '/', '*', '=');
                                 foreach (string ind in indexes)
                                 {
                                     if (ind != "")
                                     {
                                         if (char.IsLetter(ind[0]))
                                         {
-                                            _table[c, r].Data = _table[c, r].Data.Replace(ind, (string)SearchIndex(ind));
-                                            CheckCellDataType(_table[c, r].Data);
+                                            _excelTable[c, r].Data = _excelTable[c, r].Data.Replace(ind, (string)SearchIndex(ind));
+                                            CheckCellDataType(_excelTable[c, r].Data);
                                         }
                                     }
                                 }
@@ -168,69 +196,46 @@ namespace ConcoleExcelApp
             return SearchIndex(index);
         }
 
-        // calculates the resulting cells
+        // Calculates the resulting cells
         public void CalculateResult()
         {
-            for (int c = 0; c < _table.GetLength(0); c++)
+            for (int c = 0; c < _excelTable.GetColumnLength; c++)
             {
-                for (int r = 0; r < _table.GetLength(1); r++)
+                for (int r = 0; r < _excelTable.GetRowLength; r++)
                 {
-                    if (_table[c, r].DataType == CellDataType.Formula)
+                    if (_excelTable[c, r].DataType == CellDataType.Formula)
                     {
-                        if (CheckFormula(_table[c, r].Data))
+                        if (CheckFormula(_excelTable[c, r].Data))
                         {
-                            CalculateCell(_table[c, r]);
-                            /*if (_table[c, r].DataType != CellDataType.Numeric)
-                            {
-                                if (_table[c, r].Data[0] == '=')
-                                {
-                                    _table[c, r].Data = Convert.ToDouble(new DataTable().Compute(_table[c, r].Data.Substring(1), null)).ToString();
-                                }
-                                else
-                                {
-                                    _table[c, r].Data = Convert.ToDouble(new DataTable().Compute(_table[c, r].Data, null)).ToString();
-                                }
-                            }
-                            _table[c, r].DataType = CellDataType.Numeric;*/
+                            CalculateCell(_excelTable[c, r]);
                         }
                         else
                         {
-                            FindAndReplaceFormulaLinks(_table[c, r]);
-                            /*string[] indexes = _table[c, r].Data.Split('+', '-', '/', '*', '=');
-                            foreach(string ind in indexes)
-                            {
-                                if (ind != "")
-                                {
-                                    if (char.IsLetter(ind[0]))
-                                    {
-                                        _table[c, r].Data = _table[c, r].Data.Replace(ind, (string)SearchIndex(ind));
-                                        if (_table[c, r].DataType == CellDataType.Text || _table[c, r].DataType == CellDataType.Numeric)
-                                            _table[c, r].Data = _table[c, r].Data.Substring(1);
-                                    }
-                                }
-                            }*/
+                            FindAndReplaceFormulaLinks(_excelTable[c, r]);
                         }
                     }
                 }
             }
         }
 
+        // Calculates cell data
         private void CalculateCell(Cell cell)
         {
             if (cell.DataType != CellDataType.Numeric)
             {
                 if (cell.Data[0] == '=')
                 {
-                    cell.Data = Convert.ToDouble(new DataTable().Compute(cell.Data.Substring(1), null)).ToString();
+                    cell.Data = Convert.ToDouble(new DataTable().Compute(cell.Data.Substring(1), null)).ToString(CultureInfo.InvariantCulture);
                 }
                 else
                 {
-                    cell.Data = Convert.ToDouble(new DataTable().Compute(cell.Data, null)).ToString();
+                    cell.Data = Convert.ToDouble(new DataTable().Compute(cell.Data, null)).ToString(CultureInfo.InvariantCulture);
                 }
             }
             cell.DataType = CellDataType.Numeric;
         }
 
+        // Replaces links in formula with values in corresponding linked cell
         private void FindAndReplaceFormulaLinks(Cell cell)
         {
             string[] indexes = cell.Data.Split('+', '-', '/', '*', '=');
@@ -249,34 +254,36 @@ namespace ConcoleExcelApp
         }
 
 
-        // checks if cell doesn't contain any links to other cells and can be evaluated
-        public bool CheckFormula(string formula)
+        // Checks if cell doesn't contain any links to other cells and can be evaluated
+        private bool CheckFormula(string formula)
         {
-            return formula.All(c => Char.IsDigit(c) || c.Equals('+') || c.Equals('-') || 
+            return formula.All(c => char.IsDigit(c) || c.Equals('+') || c.Equals('-') || 
             c.Equals('*') || c.Equals('/') || c.Equals('=') || c.Equals('.'));
         }
 
-        // displayes the table
+        // Displayes the table
         public void DisplayTable(bool evaluated = false)
         {
-            string outstring;
             Console.Write(Environment.NewLine);
 
-            for (int c = 0; c < _table.GetLength(0); c++)
+            for (int c = 0; c < _excelTable.GetColumnLength; c++)
             {
-                for (int r = 0; r < _table.GetLength(1); r++)
+                for (int r = 0; r < _excelTable.GetRowLength; r++)
                 {
-                    if(_table[c, r].DataType == CellDataType.Text & evaluated)
-                        outstring = (_table[c, r].Data).Substring(1);
+                    string outstring;
+
+                    if(_excelTable[c, r].DataType == CellDataType.Text & evaluated)
+                        outstring = (_excelTable[c, r].Data).Substring(1);
                     else
-                        outstring = _table[c, r].Data;
+                        outstring = _excelTable[c, r].Data;
                     Console.Write($"{outstring,-15} ");
                 }
                 Console.Write(Environment.NewLine);
             }
         }
 
-        public bool ValidateUserInput(Cell cell, out string error)
+        // Validates user input
+        private bool ValidateUserInput(Cell cell, out string error)
         {
             error = "";
 
@@ -292,7 +299,8 @@ namespace ConcoleExcelApp
             return false;
         }
 
-        public bool ValidateFormula(string [] formula, out string error)
+        // Validates formula
+        private bool ValidateFormula(string [] formula, out string error)
         {
             error = "";
 
@@ -305,7 +313,8 @@ namespace ConcoleExcelApp
             return true;
         }
 
-        public bool IsValideFormula(string argument, out string error)
+        // Validates formula arguments
+        private bool IsValideFormula(string argument, out string error)
         {
             error = "";
             if (IsDigit(argument))
@@ -316,25 +325,28 @@ namespace ConcoleExcelApp
             return false;
         }
 
-        public bool IsDigit(string data)
+        // Checks if data contains digit
+        private bool IsDigit(string data)
         {
             int res;
             return int.TryParse(data, out res);
         }
 
-        public bool IsValidIndex(string index)
+        // Checks if link contains valid index
+        private bool IsValidIndex(string index)
         {
             return char.IsLetter(index[0]);
         }
 
-        public bool ValidateIndex(string index, out string error)
+        // Validates link indexes and arguments
+        private bool ValidateIndex(string index, out string error)
         {
             error = "";
 
             if (IsValidIndex(index))
             {
-                string colIndex = new String(index.TakeWhile(Char.IsLetter).ToArray());
-                string rowIndex = new String(index.SkipWhile(Char.IsLetter).ToArray());
+                string colIndex = new string(index.TakeWhile(char.IsLetter).ToArray());
+                string rowIndex = new string(index.SkipWhile(char.IsLetter).ToArray());
 
                 if (rowIndex != "")
                 {
@@ -350,14 +362,16 @@ namespace ConcoleExcelApp
             return false;
         }
 
-        public bool CheckColumnIndex(int index)
+        // Checks whether the link column index, specified by the user, exists within the table
+        private bool CheckColumnIndex(int index)
         {
-            return index <= _table.GetLength(0);
+            return index <= _excelTable.GetColumnLength;
         }
 
-        public bool CheckRowIndex(int index)
+        // Checks whether the link row index, specified by the user, exists within the table
+        private bool CheckRowIndex(int index)
         {
-            return index <= _table.GetLength(1);
+            return index <= _excelTable.GetRowLength;
         }
     }
 }
